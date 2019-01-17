3d119e14c5d9dac8cf11d3c59988683625d0fee3
Joe Sutton <hungryed6@gmail.com>
2019-01-17 15:27:01 -0800

    Add more utility calls

--- lib/sungrade_sunnova_financial/utility.rb
+++ lib/sungrade_sunnova_financial/utility.rb
@@ -8,6 +8,38 @@ module SungradeSunnovaFinancial
           req.url("#{settings.api_version}/#{config.stage}/partner/utilities")
         end
       end
+
+      def rate_plans(settings:, lead_id:, lse_id:)
+        requester = Request.new(settings: settings)
+        config = settings.configuration
+        requester.get do |req|
+          req.url("#{settings.api_version}/#{config.stage}/partner/leads/#{lead_id}/utilities/#{lse_id}/rateplans")
+        end
+      end
+
+      def current_rate_plans(settings:, lead_id:, lse_id:)
+        requester = Request.new(settings: settings)
+        config = settings.configuration
+        requester.get do |req|
+          req.url("#{settings.api_version}/#{config.stage}/partner/leads/#{lead_id}/utilities/#{lse_id}/currentrateplans")
+        end
+      end
+
+      def proposed_rate_plans(settings:, lead_id:, lse_id:)
+        requester = Request.new(settings: settings)
+        config = settings.configuration
+        requester.get do |req|
+          req.url("#{settings.api_version}/#{config.stage}/partner/leads/#{lead_id}/utilities/#{lse_id}/proposedrateplans")
+        end
+      end
+
+      def escalations(settings:, lse_id:)
+        requester = Request.new(settings: settings)
+        config = settings.configuration
+        requester.get do |req|
+          req.url("#{settings.api_version}/#{config.stage}/partner/utilities/#{lse_id}/escalations")
+        end
+      end
     end
   end
 end
